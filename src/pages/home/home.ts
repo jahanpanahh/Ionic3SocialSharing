bbb7fa369ef1633243097332ae1a3de04218c0af
Hiren Sakaria <hiren@devwork.us>
2017-06-18 21:05:32 +0530

    Added code related to social sharing with ionic3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {SocialSharing} from '@ionic-native/social-sharing';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public socShare:SocialSharing) {
 
   }
 
+  regularShare(){
+    this.socShare.share("Testing sharing via ionic app",null,"www/assets/img/hulk.jpg",null);
+  }
+
+  twitterShare(){
+    this.socShare.shareViaTwitter("Testing sharing via ionic app to twitter","www/assets/img/hulk.jpg",null);
+  }
+
+  whatsappShare(){
+    this.socShare.shareViaWhatsApp("Testing sharing via ionic app to whatsapp","www/assets/img/hulk.jpg",null);
+  }
+
+  instagramShare(){
+    this.socShare.shareViaInstagram("Testing sharing via ionic app to whatsapp","www/assets/img/hulk.jpg");
+  }
 }
